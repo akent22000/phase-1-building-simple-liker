202c8d92e7a8d8f7ffa6a143a02706fc2a22c267
amandalocher <akent22000@gmail.com>
2024-02-19 10:36:39 -0500

    completed assignment

--- main.js
+++ main.js
@@ -5,6 +5,53 @@ const FULL_HEART = '♥'
 // Your JavaScript code goes here!
 
 
+//Add the .hidden class to the error modal in the HTML so it does not appear when the page first loads
+let element = document.getElementById("modal");
+element.classList.add("hidden");
+//works!
+
+
+//When a user clicks on an empty heart:
+// Invoke mimicServerCall to simulate making a server request
+// When the "server" returns a failure status:
+// Respond to the error using a .catch(() => {}) block after your .then(() => {}) block.
+// Display the error modal by removing the .hidden class
+// Display the server error message in the modal
+// Use setTimeout to hide the modal after 3 seconds (add the .hidden class)
+// When the "server" returns a success status:
+// Change the heart to a full heart
+// Add the .activated-heart class to make the heart appear red
+const likeglyph = document.querySelectorAll(".like-glyph");
+
+
+function likeCallback(e) {
+  let heart = e.target;
+  mimicServerCall()
+    .then(function(serverMessage){
+      if ( heart.innerText === EMPTY_HEART) {
+        heart.innerText = FULL_HEART;
+        heart.className = "activated-heart";
+      } else {
+        heart.innerText = EMPTY_HEART;
+        heart.className = "";
+      }
+    })
+    .catch(function(error) {
+      const modal = document.getElementById("modal");
+      modal.className = "";
+      modal.innerText = error;
+      setTimeout(() =>  modal.className = "hidden", 3000);
+    });
+}
+
+for (const glyph of likeglyph) {
+  glyph.addEventListener("click", likeCallback);
+}
+
+
+
+
+
 
 
 //------------------------------------------------------------------------------
